b30392c3e417cc4408bcce00b69becd7f4a5fff7
James Bott <james.bott@ecoult.com>
2018-07-03 18:46:42 +1000

    defer_serve_forever parameter added to StartTcpServer()

--- pymodbus/server/sync.py
+++ pymodbus/server/sync.py
@@ -502,17 +502,24 @@ class ModbusSerialServer(object):
 # --------------------------------------------------------------------------- #
 # Creation Factories
 # --------------------------------------------------------------------------- #
-def StartTcpServer(context=None, identity=None, address=None, **kwargs):
+def StartTcpServer(context=None, identity=None, address=None, defer_serve_forever=False, **kwargs):
     """ A factory to start and run a tcp modbus server
 
     :param context: The ModbusServerContext datastore
     :param identity: An optional identify structure
     :param address: An optional (interface, port) to bind to.
     :param ignore_missing_slaves: True to not send errors on a request to a missing slave
+    :param defer_serve_forever: True/False defer running server.serve_forever() and return
+    ModbusTcpServer(), serve_forever() to be explicitly started by user.
     """
     framer = kwargs.pop("framer", ModbusSocketFramer)
     server = ModbusTcpServer(context, framer, identity, address, **kwargs)
-    server.serve_forever()
+
+    if not defer_serve_forever:
+        server.serve_forever()
+        return None
+    else:
+        return server
 
 
 def StartUdpServer(context=None, identity=None, address=None, **kwargs):
